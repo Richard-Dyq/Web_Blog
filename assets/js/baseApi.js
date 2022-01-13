3912697328457df3c9320e161e35d3a006e5d07e
RichardYQ <yq886400@dal.ca>
2022-01-13 14:36:06 +0800

    主页开发

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -2,4 +2,21 @@
 //在这个函数中， 我们可以拿提供给ajax的配置对象
 $.ajaxPrefilter(function (options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url
+    //设置有权限的借口
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    //全局统一挂载complete
+    options.complete = function (res) {
+        console.log(res);
+        //在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            //清空token
+            localStorage.removeItem('token')
+            //重新跳转到登陆页面
+            location.href = './login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+(function () {
+    //获取用户基本信息
+    getUserInfo()
+    let layer = layui.layer
+    $('#btn_logout').on('click', function () {
+        layer.confirm('你确定退出?', { icon: 3, title: '提示' }, function (index) {
+            //清空token
+            localStorage.removeItem('token')
+            //重新跳转到登陆页面
+            location.href = './login.html'
+            layer.close(index);
+        });
+    })
+
+}());
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        //请求头配置对象,配置到baseApi
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败')
+            }
+            console.log(res);
+            renderAvatar(res.data)
+        },
+        //无论成功还是失败，都会掉用complete回调函数,把该段代码放进prefilter中ß
+        // complete: function (res) {
+        //     console.log(res);
+        //     //在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+        //         //清空token
+        //         localStorage.removeItem('token')
+        //         //重新跳转到登陆页面
+        //         location.href = './login.html'
+        //     }
+        // }
+    })
+}
+function renderAvatar(user) {
+    let name = user.nickname || user.username
+    //渲染用户名
+    $('#welcome').html('欢迎,&nbsp;' + name)
+    //渲染图片头像
+    if (user.user_pic !== null) {
+        //渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        //渲染文本头像
+        $('.layui-nav-img').hide()
+        let first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
